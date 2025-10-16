fbb3553ff3cf91948e8ca999d98d657ccdbcc9da
SIMRAN-202 <kaursimrankaur2003@gmail.com>
2025-10-16 20:53:47 +0530

    updated settings file

--- LibreBooks/settings.py
+++ LibreBooks/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', 'root'),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
